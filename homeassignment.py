4ff330fca039878b47b521ed0fb27e2d5300c7b7
ferii-pourjavad <128896842+ferii-pourjavad@users.noreply.github.com>
2023-04-24 23:00:43 +0330

    Add files via upload

---
+++ homeassignment.py
@@ -0,0 +1,84 @@
+def print_board(board):
+    for i in range(len(board)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - -")
+        for j in range(len(board[0])):
+            if j % 3 == 0 and j != 0:
+                print("|", end="")
+            if j == 8:
+                print(board[i][j])
+            else:
+                print(str(board[i][j]) + " ", end="")
+
+def find_empty(board):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
+                return (i, j)
+    return None
+
+def valid(board, num, pos):
+    # Check row
+    for i in range(len(board[0])):
+        if board[pos[0]][i] == num and pos[1] != i:
+            return False
+    # Check column
+    for i in range(len(board)):
+        if board[i][pos[1]] == num and pos[0] != i:
+            return False
+    # Check 3x3 box
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+    for i in range(box_y*3, box_y*3 + 3):
+        for j in range(box_x * 3, box_x*3 + 3):
+            if board[i][j] == num and (i, j) != pos:
+                return False
+    return True
+
+def solve(board):
+    find = find_empty(board)
+    if not find:
+        return True
+    else:
+        row, col = find
+    for i in range(1, 10):
+        if valid(board, i, (row, col)):
+            board[row][col] = i
+            if solve(board):
+                return True
+            board[row][col] = 0
+    return False
+
+board = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0]
+]
+
+print_board(board)
+solve(board)
+print("\n")
+print_board(board)
+
+
+
+
+# hanoii towers
+# i=int(input("enter disk number : "))
+# def hanoii(disk_number=i,start="1",second="2",last="3"):
+#     if disk_number <= 0:
+#         print("enter valid number bigger than 1")
+#     elif disk_number==1:
+#         print("move " + " " +  start  + " " +  last )
+#     else:
+#         hanoii(disk_number-1,start,last,second)
+#         print("move" + " " +start + " " +  last)
+#         hanoii(disk_number-1,second,start,last)
+#
+# hanoii(i,"1","2","3")
